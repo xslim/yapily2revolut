46f506c8a31a05906c13602969839b4c356574ac
Taras Kalapun <t.kalapun@gmail.com>
2022-03-22 15:19:19 +0000

    Y institutions mock

---
+++ app.py
@@ -0,0 +1,44 @@
+import io
+import json
+from types import SimpleNamespace
+
+from datetime import datetime
+
+from flask import Flask
+from werkzeug.exceptions import HTTPException
+
+def json2obj(s):
+  if isinstance(s, io.IOBase):
+    s = s.read()
+  obj = json.loads(s, object_hook=lambda d: SimpleNamespace(**d))
+  return obj
+
+def obj2dict(obj):
+  d = json.loads(json.dumps(obj, default=lambda s: vars(s)))
+  return d
+
+app = Flask(__name__)
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+@app.route("/")
+def hello_world():
+  return "Hello, World!\n"
+
+@app.route("/yapily/institutions")
+def yapily_institutions():
+  f = open('yapily/institutions.json')
+  d = json2obj(f)
+  d.meta.tracingId = datetime.timestamp(datetime.now())
+  print("TracingId", d.meta.tracingId)
+  data = obj2dict(d)
+  return data
---
+++ server.sh
@@ -0,0 +1,3 @@
+!#/bin/sh
+
+flask run -h 0.0.0.0
